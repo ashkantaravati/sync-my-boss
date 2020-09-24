63c34264c6cf17ef736a591db7e57bd617310d59
a.taravati <a.taravati@papgroup>
2020-09-24 14:20:57 +0330

    fix typo in jDateTimeField

--- employee_activity/models.py
+++ employee_activity/models.py
@@ -8,6 +8,6 @@ class ActivityLog(models.Model):
     next_efforts = models.TextField()
     impediments = models.TextField()
     other_notable_feedback = models.CharField(max_length=200)
-    from_time = jmodels.jdateTimeField()
-    to_time = jmodels.jdateTimeField()
+    from_time = jmodels.jDateTimeField
+    to_time = jmodels.jDateTimeField()
 
