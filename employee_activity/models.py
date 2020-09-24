f49a95b0836153a68d27454a620189f07837e084
a.taravati <a.taravati@papgroup>
2020-09-24 14:18:32 +0330

    implement basic model for activity log

---
+++ employee_activity/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django_jalali.db import models as jmodels
+
+class ActivityLog(models.Model):
+    objects = jmodels.jManager()
+    employee = models.CharField(max_length=200)
+    previous_efforts = models.TextField()
+    next_efforts = models.TextField()
+    impediments = models.TextField()
+    other_notable_feedback = models.CharField(max_length=200)
+    from_time = jmodels.jdateTimeField()
+    to_time = jmodels.jdateTimeField()
+
