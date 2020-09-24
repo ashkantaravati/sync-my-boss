865d064a3d90535af328f1026a2a3dcf6553bda3
a.taravati <a.taravati@papgroup>
2020-09-24 14:50:06 +0330

    implement basic function view for getting all logs

--- employee_activity/views.py
+++ employee_activity/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import ActivityLog
 
-# Create your views here.
+
+def all_activities(request):
+    if request.method == "GET":
+        activities = ActivityLog.objects.all() #.order_by("-datetime_submitted")
+        return render(
+            request,
+            "employee_activity/all-activities.html",
+            {"activities": activities},
+        )
