d580710e6facd84f2833c6716f8d31a5104baef4
ashkantaravati <ashkan.taravati@gmail.com>
2020-09-30 15:44:15 +0330

    use vue and axios to fetch and render logs

---
+++ assets/js/dashboard.js
@@ -0,0 +1,23 @@
+const timeline = {
+    delimiters: ['[[', ']]'],
+    data() {
+        return {
+          counter: 0,
+          logs: [],
+        }
+      },
+      mounted() {
+        setInterval(() => {
+          this.counter++;
+        //   axios.get('/api/logs', { params: { id: id }})
+        axios.get('/api/logs')
+        .then(response => {
+          console.log(response);
+          this.logs = response.data;
+        }).catch(error => {
+          console.log(response)
+        })
+        }, 5000)
+    }
+  }
+  Vue.createApp(timeline).mount("#timeline");
