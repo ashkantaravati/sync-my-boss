6a7adb18f0a301d0f6fa6409879e2786b89dae4d
ashkantaravati <ashkan.taravati@gmail.com>
2020-09-30 17:29:42 +0330

    use nested serializers to include employee

--- employee_activity/serializers.py
+++ employee_activity/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
-from .models import Log
+from .models import Log, Employee
+
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = '__all__'
 
 
 class LogSerializer(serializers.ModelSerializer):
+    employee = EmployeeSerializer(read_only=True)
+    
     class Meta:
         model = Log
         fields = ["id", "datetime_occured", "employee", "event_message", "event_type"]
