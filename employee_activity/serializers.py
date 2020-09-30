d3b91795198070f9f392488334c6011aba2c0c0a
ashkantaravati <ashkan.taravati@gmail.com>
2020-09-30 22:39:44 +0330

    full_name and formatted datetime

--- employee_activity/serializers.py
+++ employee_activity/serializers.py
@@ -5,12 +5,18 @@ from .models import Log, Employee
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
-        fields = '__all__'
+        fields = ["first_name", "last_name", "job_title", "user", "full_name"]
 
 
 class LogSerializer(serializers.ModelSerializer):
     employee = EmployeeSerializer(read_only=True)
-    
+
     class Meta:
         model = Log
-        fields = ["id", "datetime_occured", "employee", "event_message", "event_type"]
+        fields = [
+            "id",
+            "datetime_occured_formatted",
+            "employee",
+            "event_message",
+            "event_type",
+        ]
