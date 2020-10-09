ac3e3ad9fcead2a98508315775735dd9d19a7f61
ashkantaravati <ashkan.taravati@gmail.com>
2020-10-10 00:38:58 +0330

    remove redundant lines

--- employee_activity/views.py
+++ employee_activity/views.py
@@ -1,18 +1,13 @@
 from django.shortcuts import render
-from .serializers import LogSerializer, AvailabilityStatusSerializer, EmployeeSerializer
-from .models import Log, AvailabilityStatus, Employee
-from rest_framework import generics
 from .utils import get_today_formatted_jdatetime
 
 
 def dashboard(request):
     if request.method == "GET":
         if request.user.is_authenticated:
-            # Do something for authenticated users.
             current_user = request.user
             current_employee = current_user.employee
             current_employee_id = current_employee.pk
-            today = datetime.now().strftime("%a, %d %b %Y")
             active_coworkers = 21
 
             return render(
