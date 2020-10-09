1b5e8dbf706deecd948b33c37c733316283fb5e5
ashkantaravati <ashkan.taravati@gmail.com>
2020-10-10 00:34:58 +0330

    improve get_formatted_jdatetime and get_today

--- employee_activity/utils.py
+++ employee_activity/utils.py
@@ -1,4 +1,4 @@
-from jdatetime import datetime
+from jdatetime import datetime, date
 
 
 def get_kebab_case(string):
@@ -8,14 +8,28 @@ def get_kebab_case(string):
     return kebab_cased
 
 
-def get_formatted_jdatetime(datetime_object):
-    jobject = datetime(
-        datetime_object.year,
-        datetime_object.month,
-        datetime_object.day,
-        datetime_object.hour,
-        datetime_object.minute,
-        datetime_object.second,
-        locale='fa_IR'
-    )
-    return jobject.strftime("%a, %d %b %Y %H:%M:%S")
+def get_formatted_jdatetime(datetime_object, object_type="datetime", show_seconds=True):
+    year = datetime_object.year
+    month = datetime_object.month
+    day = datetime_object.day
+    if object_type.lower() == "datetime":
+        jobject = datetime(
+            year,
+            month,
+            day,
+            datetime_object.hour,
+            datetime_object.minute,
+            datetime_object.second,
+            locale="fa_IR",
+        )
+        format_rule = "%a, %d %b %Y %H:%M:%S" if show_seconds else "%a, %d %b %Y %H:%M"
+        return jobject.strftime(format_rule)
+    elif object_type.lower() == "date":
+        jobject = date(year, month, day, locale="fa_IR")
+        return jobject.strftime("%a, %d %b %Y")
+    else:
+        raise ValueError("Unexpected argument was passed")
+
+
+def get_today_formatted_jdatetime():
+    return get_formatted_jdatetime(date.today(), object_type="Date")
